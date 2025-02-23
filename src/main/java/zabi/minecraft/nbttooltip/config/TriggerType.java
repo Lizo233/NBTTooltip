97a52281b550cb3b3e1da7276c206eebe473b4af
Lizo233 <Lizo233@proton.me>
2025-02-23 22:29:16 +0800

    YES2

--- src/main/java/zabi/minecraft/nbttooltip/config/TriggerType.java
+++ src/main/java/zabi/minecraft/nbttooltip/config/TriggerType.java
@@ -3,7 +3,7 @@ package zabi.minecraft.nbttooltip.config;
 import java.util.function.BiFunction;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.TooltipType;
+import net.minecraft.client.itemTooltipType;
 import zabi.minecraft.nbttooltip.NBTTooltip;
 
 public enum TriggerType {
